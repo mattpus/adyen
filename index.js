944bb6147ae3fd64c282751ed676712359b347b8
Mateusz <mateusz.pusiewicz@ing.com>
2022-12-30 17:01:53 +0100

    init

---
+++ index.js
@@ -0,0 +1,155 @@
+const express = require("express");
+const path = require("path");
+const hbs = require("express-handlebars");
+const dotenv = require("dotenv");
+const morgan = require("morgan");
+const { uuid } = require("uuidv4");
+
+const { hmacValidator } = require('@adyen/api-library');
+const { Client, Config, CheckoutAPI } = require("@adyen/api-library");
+
+// init app
+const app = express();
+// setup request logging
+app.use(morgan("dev"));
+// Parse JSON bodies
+app.use(express.json());
+// Parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+// Serve client from build folder
+app.use(express.static(path.join(__dirname, "/public")));
+
+// enables environment variables by
+// parsing the .env file and assigning it to process.env
+dotenv.config({
+  path: "./.env",
+});
+
+// Adyen Node.js API library boilerplate (configuration, etc.)
+const config = new Config();
+config.apiKey = process.env.ADYEN_API_KEY;
+const client = new Client({ config });
+client.setEnvironment("TEST");  // change to LIVE for production
+const checkout = new CheckoutAPI(client);
+
+app.engine(
+  "handlebars",
+  hbs.engine({
+    defaultLayout: "main",
+    layoutsDir: __dirname + "/views/layouts",
+    helpers: require("./util/helpers"),
+  })
+);
+
+app.set("view engine", "handlebars");
+
+/* ################# API ENDPOINTS ###################### */
+
+// Invoke /sessions endpoint
+app.post("/api/sessions", async (req, res) => {
+
+  try {
+    // unique ref for the transaction
+    const orderRef = uuid();
+    // Allows for gitpod support
+    const localhost = req.get('host');
+    // const isHttps = req.connection.encrypted;
+    const protocol = req.socket.encrypted? 'https' : 'http';
+    // Ideally the data passed here should be computed based on business logic
+    const response = await checkout.sessions({
+      amount: { currency: "EUR", value: 10000 }, // value is 100€ in minor units
+      countryCode: "NL",
+      merchantAccount: process.env.ADYEN_MERCHANT_ACCOUNT, // required
+      reference: orderRef, // required: your Payment Reference
+      returnUrl: `${protocol}://${localhost}/checkout?orderRef=${orderRef}`, // set redirect URL required for some payment methods (ie iDEAL)
+      // set lineItems required for some payment methods (ie Klarna)
+      lineItems: [
+        {quantity: 1, amountIncludingTax: 5000 , description: "Sunglasses"},
+        {quantity: 1, amountIncludingTax: 5000 , description: "Headphones"}
+      ] 
+    });
+
+    res.json(response);
+  } catch (err) {
+    console.error(`Error: ${err.message}, error code: ${err.errorCode}`);
+    res.status(err.statusCode).json(err.message);
+  }
+});
+
+
+/* ################# end API ENDPOINTS ###################### */
+
+/* ################# CLIENT SIDE ENDPOINTS ###################### */
+
+// Index (select a demo)
+app.get("/", (req, res) => res.render("index"));
+
+// Cart (continue to checkout)
+app.get("/preview", (req, res) =>
+  res.render("preview", {
+    type: req.query.type,
+  })
+);
+
+// Checkout page (make a payment)
+app.get("/checkout", (req, res) =>
+  res.render("checkout", {
+    type: req.query.type,
+    clientKey: process.env.ADYEN_CLIENT_KEY
+  })
+);
+
+// Result page
+app.get("/result/:type", (req, res) =>
+  res.render("result", {
+    type: req.params.type,
+  })
+);
+
+/* ################# end CLIENT SIDE ENDPOINTS ###################### */
+
+/* ################# WEBHOOK ###################### */
+
+app.post("/api/webhooks/notifications", async (req, res) => {
+
+  // YOUR_HMAC_KEY from the Customer Area
+  const hmacKey = process.env.ADYEN_HMAC_KEY;
+  const validator = new hmacValidator()
+  // Notification Request JSON
+  const notificationRequest = req.body;
+  const notificationRequestItems = notificationRequest.notificationItems
+
+  // Handling multiple notificationRequests
+  notificationRequestItems.forEach(function(notificationRequestItem) {
+
+    const notification = notificationRequestItem.NotificationRequestItem
+
+    // Handle the notification
+    if( validator.validateHMAC(notification, hmacKey) ) {
+      // Process the notification based on the eventCode
+        const merchantReference = notification.merchantReference;
+        const eventCode = notification.eventCode;
+        console.log('merchantReference:' + merchantReference + " eventCode:" + eventCode);
+      } else {
+        // invalid hmac: do not send [accepted] response
+        console.log("Invalid HMAC signature: " + notification);
+        res.status(401).send('Invalid HMAC signature');
+    }
+});
+
+  res.send('[accepted]')
+});
+
+
+/* ################# end WEBHOOK ###################### */
+
+/* ################# UTILS ###################### */
+
+function getPort() {
+  return process.env.PORT || 8080;
+}
+
+/* ################# end UTILS ###################### */
+
+// Start server
+app.listen(getPort(), () => console.log(`Server started -> http://localhost:${getPort()}`));
